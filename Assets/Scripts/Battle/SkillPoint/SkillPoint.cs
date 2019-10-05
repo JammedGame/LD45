f2e8842a84860bc75fcfc019e2df7a3982ca83b6
Nikola <glamdring0@hotmail.com>
2019-10-05 19:52:27 +0200

    SkillPoint_v2

--- Assets/Scripts/Battle/SkillPoint/SkillPoint.cs
+++ Assets/Scripts/Battle/SkillPoint/SkillPoint.cs
@@ -3,12 +3,21 @@ using Unity.Mathematics;
 public class SkillPoint : BattleObject
 {
     public float Health;
-	public SkillPointSettings SkillPointSettings;
+    public SkillPointSettings SkillPointSettings;
 
     public SkillPoint(Room room, SkillPointSettings settings, OwnerId owner, float2 position) : base(room, settings, owner, position)
     {
         Health = settings.Health;
-		SkillPointSettings = settings;
+        SkillPointSettings = settings;
+    }
+
+    public override void OnCollisionWith(BattleObject other)
+    {
+        if(other is Player player) {
+            player.SkillPoints++;
+            this.Deactivate();
+        }
+
     }
 
 }
