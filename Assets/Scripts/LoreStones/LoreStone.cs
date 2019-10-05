2211982a1d733f7db1ba9267ebdfa4af1d08aa93
DIXY <dixyye@gmail.com>
2019-10-05 19:11:30 +0200

    lore prep, randomize room positions

---
+++ Assets/Scripts/LoreStones/LoreStone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class LoreStone : BattleObject
+{
+    public readonly LoreStoneSettings LoreSettings;
+
+    public LoreStone(Room room, LoreStoneSettings settings, float2 position) : base(room, settings, OwnerId.Neutral, position)
+    {
+        LoreSettings = settings;
+    }
+}
