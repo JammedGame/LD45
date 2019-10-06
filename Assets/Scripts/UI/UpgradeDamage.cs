a799d6150ee6724193c72d596cfb1b38194b94c0
Nikola <glamdring0@hotmail.com>
2019-10-06 17:57:04 +0200

    SkillPoints-show upgrade

---
+++ Assets/Scripts/UI/UpgradeDamage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeDamage : MonoBehaviour
+{
+    public Image image;
+    
+    public void ShowImage(){
+        image.gameObject.SetActive(true);
+    }
+
+        public void HideImage(){
+        image.gameObject.SetActive(false);
+    }
+}
