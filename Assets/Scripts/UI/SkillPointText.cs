e71fddd5ede07a71a56144537c196c463ed3a035
Nikola <glamdring0@hotmail.com>
2019-10-06 14:28:25 +0200

    SkillPointCounter

---
+++ Assets/Scripts/UI/SkillPointText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillPointText : MonoBehaviour
+{
+    public Text SkillPointCount;
+
+    public void Update(){
+        SkillPointCount.text = Game.Player.SkillPoints.ToString();
+    }
+}
