893f217da3310e7f042445dcd0da69c5244b5244
Misa Jovanovic <misaj@nordeus.com>
2019-10-07 21:02:46 +0200

    light 2d

--- Assets/Scripts/BattleVisuals/ObjectVisuals/PlayerVisual.cs
+++ Assets/Scripts/BattleVisuals/ObjectVisuals/PlayerVisual.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 [RequireComponent(typeof(Animator))]
 public class PlayerVisual : BattleObject3D
 {
+    public Light2D Light2D;
+
     public override void Sync(float dT)
     {
         base.Sync(dT);
-        
+
         var player = (Player)Data;
         GetComponent<Animator>().SetInteger("ActionType", (int)player.CurrentAnimation);
     }
