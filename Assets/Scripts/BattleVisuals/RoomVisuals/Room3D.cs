563a21d4e0937201892dd5f9fe031a9d38f43d40
Manojlovic Milos <manojlovicsmilos@hotmail.com>
2019-10-07 16:49:27 +0200

    Door Locks

--- Assets/Scripts/BattleVisuals/RoomVisuals/Room3D.cs
+++ Assets/Scripts/BattleVisuals/RoomVisuals/Room3D.cs
@@ -6,6 +6,10 @@ public class Room3D : MonoBehaviour
 	public GameObject NorthDoor;
 	public GameObject WestDoor;
 	public GameObject SouthDoor;
+	public GameObject EastDoorLocked;
+	public GameObject NorthDoorLocked;
+	public GameObject WestDoorLocked;
+	public GameObject SouthDoorLocked;
 	public GameObject Walls;
 	public Sprite Frame1;
 	public Sprite Frame2;
@@ -34,12 +38,37 @@ public class Room3D : MonoBehaviour
 		if(room.World.GameWorldData.LevelId == 2) instance.Floor.GetComponent<SpriteRenderer>().sprite = instance.Floor2;
 		if(room.World.GameWorldData.LevelId == 3) instance.Floor.GetComponent<SpriteRenderer>().sprite = instance.Floor3;
 
-		instance.NorthDoor.SetActive(room.NextRoom != null && room.NextRoom.Y == room.Y + 1 );
-		instance.EastDoor.SetActive(room.NextRoom != null && room.NextRoom.X == room.X + 1);
-
-		instance.WestDoor.SetActive(room.PreviousRoom != null && room.PreviousRoom.X == room.X - 1 );
-		instance.SouthDoor.SetActive(room.PreviousRoom != null && room.PreviousRoom.Y == room.Y - 1);		
+		instance.CheckDoors(room.CanProgressToNextRoom());
 
 		return instance;
+	}
+	public void Update()
+	{
+		CheckDoors(room.CanProgressToNextRoom());
+	}
+	public void CheckDoors(bool Locked)
+	{
+		if(Locked)
+		{
+			this.NorthDoor.SetActive(room.NextRoom != null && room.NextRoom.Y == room.Y + 1 );
+			this.EastDoor.SetActive(room.NextRoom != null && room.NextRoom.X == room.X + 1);
+			this.WestDoor.SetActive(room.PreviousRoom != null && room.PreviousRoom.X == room.X - 1 );
+			this.SouthDoor.SetActive(room.PreviousRoom != null && room.PreviousRoom.Y == room.Y - 1);
+			this.NorthDoorLocked.SetActive(false);
+			this.SouthDoorLocked.SetActive(false);
+			this.WestDoorLocked.SetActive(false);
+			this.EastDoorLocked.SetActive(false);
+		}
+		else
+		{
+			this.NorthDoorLocked.SetActive(room.NextRoom != null && room.NextRoom.Y == room.Y + 1 );
+			this.EastDoorLocked.SetActive(room.NextRoom != null && room.NextRoom.X == room.X + 1);
+			this.WestDoorLocked.SetActive(room.PreviousRoom != null && room.PreviousRoom.X == room.X - 1 );
+			this.SouthDoorLocked.SetActive(room.PreviousRoom != null && room.PreviousRoom.Y == room.Y - 1);
+			this.NorthDoor.SetActive(false);
+			this.SouthDoor.SetActive(false);
+			this.WestDoor.SetActive(false);
+			this.EastDoor.SetActive(false);
+		}
 	}
 }
