984a45d5120b7d7189386fe37e3def2fbe8a3db0
Manojlovic Milos <manojlovicsmilos@hotmail.com>
2019-10-08 11:50:15 +0200

    Boss 2 Fixes

--- Assets/Scripts/Battle/Mobs/Boss2.cs
+++ Assets/Scripts/Battle/Mobs/Boss2.cs
@@ -8,6 +8,7 @@ using System;
 public class Boss2 : Mob
 {
     private float _SpawnProgress;
+    private float _BeamProgress = 0;
     public Boss2(MobSettings mobSettings, Room room, float2 position) : base(mobSettings, room, position)
 	{
 	}
@@ -24,6 +25,7 @@ public class Boss2 : Mob
 		Velocity *= 0.98f;
 		AttackProgress -= dT;
         _SpawnProgress -= dT;
+        _BeamProgress -= dT;
 
         // odmrsti se
         if (_SpawnProgress < 4f)
@@ -34,11 +36,11 @@ public class Boss2 : Mob
         if(_SpawnProgress > 0) return;
 
 		var distanceToPlayer = DistanceToPlayer;
-		if(UnityEngine.Random.Range(0f, 1f) < 0.8f)
+		if(UnityEngine.Random.Range(0f, 1f) < 0.4f || _BeamProgress > 0)
         {
             SpawnMob();
         }
-        else
+        else if(_BeamProgress <= 0)
         {
             SpawnBeam();
         }
@@ -49,17 +51,19 @@ public class Boss2 : Mob
 
 	private void SpawnBeam()
 	{
-        var pos = Room.World.GameWorldData.MobSpawnPoints.GetRandom() * 2;
+        _BeamProgress = 3;
         var marusBeamSettings = new ProjectileSettings();
         marusBeamSettings.name = "MarusBeam";
-        marusBeamSettings.Size = new float2(0.3f, 0.3f);
-        marusBeamSettings.IsStatic = true;
-        new MarusBeam(Room, marusBeamSettings, this, pos);
+        marusBeamSettings.Size = new float2(0.5f, 0.5f);
+        foreach(var pos in Room.World.GameWorldData.MobSpawnPoints)
+        {
+            new MarusBeam(Room, marusBeamSettings, this, new Vector2(pos.x * 1.4f, pos.y * 1.4f));
+        }
 	}
 
 	private void SpawnMob()
     {
-        _SpawnProgress = 5;
+        _SpawnProgress = 1.5f;
         CurrentAnimation = AnimationType.Minion;
         MobSettings.Minion.SpawnIntoRoom(Room, new float2(Position.x, Position.y - 2f));
     }
@@ -68,12 +72,29 @@ public class Boss2 : Mob
 
 public class MarusBeam : BattleObject
 {
+    private float _Length;
     public readonly Boss2 Marus;
 
 	public MarusBeam(Room room, BattleObjectSettings settings, Boss2 parent, float2 position) : base(room, settings, parent, position)
 	{
+        _Length = 3;
         Marus = parent;
 	}
+
+    protected override void OnAct(float dT)
+    {
+        base.OnAct(dT);
+
+        _Length -= dT;
+        if(_Length < 0)
+        {
+            Deactivate();
+            return;
+        }
+
+        Position.x *= 1.005f;
+        Position.y *= 1.005f;
+    }
 
     public override void OnCollisionWith(BattleObject other)
     {
